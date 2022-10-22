1546e3b40d8eb410bb5cb0aa7763f03b6e41f54a
Sergii Karpus <karpus.sergey@gmail.com>
2022-10-22 11:42:29 +0300

    Done task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,16 +1,12 @@
 // Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
-const xxx = document.querySelector('#name-input');
+const inputName = document.querySelector('#name-input');
+const spanOutput = document.querySelector("#name-output");
 
-let stringSpan = "";
-const userName = (event) => {
-   // console.log(event);
-   // if (event.key === "Backspase") {
-   //    stringSpan.substring(0, stringSpan.length - 1);  
-   // }else {
-      stringSpan += event.key;
-      document.querySelector("#name-output").textContent = stringSpan;
-      console.log(stringSpan);
-   // }
+const userName = () => {
+   if (inputName.value.length == 0) {
+      spanOutput.textContent = "Anonymous";
+   } else {
+      spanOutput.textContent = inputName.value;
+   }   
 }
-
-xxx.addEventListener("keydown", userName);
+inputName.addEventListener("keyup", userName);
